87e291918d39c98b0a0151cd3b5bd034be6eb25d
hagiss <66506438+hagiss@users.noreply.github.com>
2023-01-27 21:13:45 +0900

    asd'

--- tools/lazyconfig_train_net.py
+++ tools/lazyconfig_train_net.py
@@ -61,7 +61,7 @@ def do_train(args, cfg):
                 checkpointer (dict)
                 ddp (dict)
     """
-    cfg.dataloader.train.total_batch_size = 16
+    cfg.dataloader.train.total_batch_size = 1
     cfg.model.roi_heads.batch_size_per_image = 16
     cfg.model.roi_heads.num_classes = 10
     for i in cfg.keys():
