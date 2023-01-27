0b2f875a571217a855df36bdfa602944ab003a67
hagiss <66506438+hagiss@users.noreply.github.com>
2023-01-27 19:53:22 +0900

    asd

--- tools/lazyconfig_train_net.py
+++ tools/lazyconfig_train_net.py
@@ -202,9 +202,45 @@ def do_train(args, cfg):
     # cfg.dataloader.train.dataset.names = "coco_trash_train"
     # cfg.dataloader.test.dataset.names = "coco_trash_test"
     # print(cfg.dataloader.train)
+    # -----------------------------------------------------------------------------
+    # Dataset
+    # -----------------------------------------------------------------------------
     cfg.DATASETS = CN()
+    # List of the dataset names for training. Must be registered in DatasetCatalog
+    # Samples from these datasets will be merged and used as one dataset.
     cfg.DATASETS.TRAIN = ('coco_trash_train',)
+    # List of the pre-computed proposal files for training, which must be consistent
+    # with datasets listed in DATASETS.TRAIN.
+    cfg.DATASETS.PROPOSAL_FILES_TRAIN = ()
+    # Number of top scoring precomputed proposals to keep for training
+    cfg.DATASETS.PRECOMPUTED_PROPOSAL_TOPK_TRAIN = 2000
+    # List of the dataset names for testing. Must be registered in DatasetCatalog
     cfg.DATASETS.TEST = ('coco_trash_test',)
+    # List of the pre-computed proposal files for test, which must be consistent
+    # with datasets listed in DATASETS.TEST.
+    cfg.DATASETS.PROPOSAL_FILES_TEST = ()
+    # Number of top scoring precomputed proposals to keep for test
+    cfg.DATASETS.PRECOMPUTED_PROPOSAL_TOPK_TEST = 1000
+
+    # -----------------------------------------------------------------------------
+    # DataLoader
+    # -----------------------------------------------------------------------------
+    cfg.DATALOADER = CN()
+    # Number of data loading threads
+    cfg.DATALOADER.NUM_WORKERS = 4
+    # If True, each batch should contain only images for which the aspect ratio
+    # is compatible. This groups portrait images together, and landscape images
+    # are not batched with portrait images.
+    cfg.DATALOADER.ASPECT_RATIO_GROUPING = True
+    # Options: TrainingSampler, RepeatFactorTrainingSampler
+    cfg.DATALOADER.SAMPLER_TRAIN = "TrainingSampler"
+    # Repeat threshold for RepeatFactorTrainingSampler
+    cfg.DATALOADER.REPEAT_THRESHOLD = 0.0
+    # Tf True, when working on datasets that have instance annotations, the
+    # training dataloader will filter out images without associated annotations
+    cfg.DATALOADER.FILTER_EMPTY_ANNOTATIONS = True
+
+    # -----------------------------------------------------------------------------
 
     # train_loader = instantiate(cfg.dataloader.train)
     train_loader = build_detection_train_loader(
